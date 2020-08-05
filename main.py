99d1afacab04b08146cd504b4fb46bd7701f786c
Advait Joshi <advaitjoshi1998@gmail.com>
2020-08-05 17:19:45 +0300

    Add download progress bar

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from pytube import YouTube
 import argparse
+import sys
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    filesize = stream.filesize
+    bytes_received = filesize - bytes_remaining
+
+    ch = '█'
+
+    done = int(bytes_received / filesize * 20)
+    remaining = int(bytes_remaining / filesize * 20)
+
+    bar = ch * done + ' ' * remaining
+    percent = round(100.0 * bytes_received / float(filesize), 1)
+    text = f' -> |{bar}| {percent}%\r'
+
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', required=True)
     args = parser.parse_args()
 
-    yt = YouTube(args.url)
+    yt = YouTube(args.url, on_progress_callback=on_progress)
 
     streams_list = yt.streams.filter(file_extension='mp4', progressive=True)
     print("Available videos:")
